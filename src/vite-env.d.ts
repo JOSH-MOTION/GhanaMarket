872e7b0ff5a54a5a3e9b705515779300e2d26c2b
JOSH-MOTION <joshuadoe168@gmail.com>
2025-10-27 12:41:22 +0000

    workig

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string
+  readonly VITE_FIREBASE_PROJECT_ID: string
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly VITE_FIREBASE_APP_ID: string
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string
+
+  // ✅ Add these for Cloudinary
+  readonly VITE_CLOUDINARY_CLOUD_NAME: string
+  readonly VITE_CLOUDINARY_UNSIGNED_PRESET: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
